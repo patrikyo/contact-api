03ad40eabca6323589fbc1f23fc2df150884ca75
youpa <patrik.youssef@arbetsformedlingen.se>
2025-06-12 08:57:15 +0200

    Adds cors and whitelisting

--- dist/app.js
+++ dist/app.js
@@ -4,9 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const contactRoutes_1 = __importDefault(require("./routes/contactRoutes"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3100;
+app.use((0, cors_1.default)({
+    origin: "http://localhost:3000",
+}));
 app.use(express_1.default.json());
 app.use("/api/contacts", contactRoutes_1.default);
 app.listen(PORT, () => {
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from "cors";
 import contactRoutes from "./routes/contactRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 3100;
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 app.use("/api/contacts", contactRoutes);
