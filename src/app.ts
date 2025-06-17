f8fd0350483981b9ef15854bfb5d0c8937de5d49
youpa <patrik.youssef@arbetsformedlingen.se>
2025-06-17 22:17:09 +0200

    Adds utilization of .env file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import express from "express";
 import cors from "cors";
 import contactRoutes from "./routes/contactRoutes";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.buifg5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 const PORT = process.env.PORT || 3100;
